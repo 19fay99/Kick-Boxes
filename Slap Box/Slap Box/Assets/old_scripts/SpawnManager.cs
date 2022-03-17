44d0c15061a5962bac8113f0c4bf7d6a527976d9
wildodyssey <adambrochu13@gmail.com>
2022-03-17 14:07:20 -0400

    script

---
+++ Slap Box/Slap Box/Assets/old_scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] RainPrefabs;
+     private float spawnRangex = -1;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnCloud", startDelay, spawnInterval);
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnCloud()
+    {
+        int rainprefabIndex = Random.Range(0, RainPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), -12.19709f, spawnPosZ);
+
+        Instantiate(RainPrefabs[rainprefabIndex], spawnPos, RainPrefabs[rainprefabIndex].transform.rotation);
+    }
+}
