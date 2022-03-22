7afcb7b7d8bb2499fa25c54bb099393539cf73c1
Andrew Fay Matseas <andrewafay@gmail.com>
2022-03-22 14:03:07 -0400

    Quit

--- Slap Box/Slap Box/Assets/Scripts/Quit.cs
+++ Slap Box/Slap Box/Assets/Scripts/Quit.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
