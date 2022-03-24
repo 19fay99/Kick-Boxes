950438b461f945c4b71d8603d29a68fe4089577b
Andrew Fay Matseas <andrewafay@gmail.com>
2022-03-24 15:14:47 -0400

    Added new crate with allocated point difference

---
+++ Slap Box/Slap Box/Assets/Scripts/MetalCratePoints.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalCratePoints : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "GreenShip")
+        {
+            gameManager.UpdateP1Score(3);
+            Destroy(gameObject);
+        }
+
+        if (collision.transform.name == "PurpleShip")
+        {
+            gameManager.UpdateP2Score(3);
+            Destroy(gameObject);
+        }
+    }
+}
--- Slap Box/Slap Box/Assets/Scripts/MetalCrateSpawner.cs
+++ Slap Box/Slap Box/Assets/Scripts/MetalCrateSpawner.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrateSpawnManager : MonoBehaviour
+public class MetalCrateSpawner : MonoBehaviour
 {
     public GameObject[] Prefabs;
-    private float startDelay = 1;
-    public float spawnInterval = 5;
+    private float startDelay = 20;
+    public float spawnInterval = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +23,7 @@ public class CrateSpawnManager : MonoBehaviour
     void SpawnCrate()
     {
         int PrefabIndex = Random.Range(0, Prefabs.Length);
-        Vector2 spawnPos = new Vector2(0,0);
+        Vector2 spawnPos = new Vector2(0, 0);
 
         Instantiate(Prefabs[PrefabIndex], spawnPos, Prefabs[PrefabIndex].transform.rotation);
     }
